c6f104ae829ffd20b65c4ce2e8c577e7a3a70500
Abhijeet <43561915+Vatsha@users.noreply.github.com>
2020-08-15 23:22:54 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponents,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
